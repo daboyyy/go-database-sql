c9e1ec3df614a6cb6fb8d2d99d53889de51fe85c
jiniusz <dev.jiniusz@gmail.com>
2022-03-12 05:15:48 +0700

    fix: replace some func with sqlx

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
 	"fmt"
 
 	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
 type Cover struct {
@@ -14,7 +13,7 @@ type Cover struct {
 	Name string
 }
 
-var db *sql.DB
+var db *sqlx.DB
 
 func main() {
 	var err error
@@ -23,29 +22,8 @@ func main() {
 	// db, err = sql.Open("sqlserver", "sqlserver://sa:P@ssw0rd@13.76.163.73?database=techcoach")
 
 	// MySQL
-	db, err = sql.Open("mysql", "root:P@ssw0rd@tcp(13.76.163.73)/techcoach")
+	db, err = sqlx.Open("mysql", "root:P@ssw0rd@tcp(13.76.163.73)/techcoach")
 
-	if err != nil {
-		panic(err)
-	}
-
-	// AddCover
-	/* cover := Cover{9, "Bond"}
-	err = AddCover(cover)
-	if err != nil {
-		panic(err)
-	} */
-
-	// UpdateCover
-	/* cover := Cover{9, "Hello"}
-	err = UpdateCover(cover)
-	if err != nil {
-		panic(err)
-	} */
-
-	// DeleteCover
-	deleteId := 9
-	err = DelteCover(deleteId)
 	if err != nil {
 		panic(err)
 	}
@@ -60,121 +38,26 @@ func main() {
 	for _, cover := range covers {
 		fmt.Println(cover)
 	}
-
-	// GetCover
-	/* cover, err := GetCover(1)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(cover) */
 }
 
 func GetCovers() ([]Cover, error) {
-	err := db.Ping()
-	if err != nil {
-	 	return nil, err
-	}
-
 	query := "select id, name from cover"
-	rows, err := db.Query(query)
+	covers := []Cover{}
+	err := db.Select(&covers, query)
 	if err != nil {
 		return nil, err
-	}
-	defer rows.Close() // promise if program end this will be close
-
-	covers := []Cover{}
-	for rows.Next() {
-		cover := Cover{}
-
-		err = rows.Scan(&cover.Id, &cover.Name)
-		if err != nil {
-			return nil, err
-		}
-
-		covers = append(covers, cover)
 	}
 
 	return covers, nil
 }
 
 func GetCover(id int) (*Cover, error) {
-	err := db.Ping()
-	if err != nil {
-		return nil, err
-	}
-
-	// MSSQL Server
-	// query := "select id, name from cover where id=@id"
-
-	// MySQL
 	query := "select id, name from cover where id=?"
-
-	row := db.QueryRow(query, id)
 	cover := Cover{}
-	err = row.Scan(&cover.Id, &cover.Name)
+	err := db.Get(&cover, query, id)
 	if err != nil {
 		return nil, err
 	}
 
 	return &cover, nil
-}
-
-func AddCover(cover Cover) error {
-	// MySQL
-	query := "insert into cover (id, name) values (?, ?)" // no auto increment have to add id
-	result, err := db.Exec(query, cover.Id, cover.Name)
-	if err != nil {
-		return err
-	}
-
-	affected, err := result.RowsAffected()
-	if err != nil {
-		return err
-	}
-
-	if affected <= 0 {
-		return errors.New("cannot insert")
-	}
-
-	return nil
-}
-
-func UpdateCover(cover Cover) error {
-	// MySQL
-	query := "update cover set name=? where id=?"
-	result, err := db.Exec(query, cover.Name, cover.Id)
-	if err != nil {
-		return err
-	}
-
-	affected, err := result.RowsAffected()
-	if err != nil {
-		return err
-	}
-
-	if affected <= 0 {
-		return errors.New("cannot update")
-	}
-
-	return nil
-}
-
-func DelteCover(id int) error {
-	// MySQL
-	query := "delete from cover where id=?"
-	result, err := db.Exec(query, id)
-	if err != nil {
-		return err
-	}
-
-	affected, err := result.RowsAffected()
-	if err != nil {
-		return err
-	}
-
-	if affected <= 0 {
-		return errors.New("cannot delete")
-	}
-
-	return nil
 }
